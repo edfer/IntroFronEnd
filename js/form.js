07bcb71be268ae092dc43f6add8cf7f34c33de50
ed_fer <eduardojfernandezb@gmail.com>
2016-07-07 18:27:57 +0200

    scroll añadido

--- js/form.js
+++ js/form.js
@@ -1,4 +1,3 @@
-
 var form = document.getElementById("form-contact");
 var requireService = document.getElementById("require-service");
 
@@ -9,24 +8,66 @@ var noNeedVideo = document.getElementById("services-no");
 
 needVideo.addEventListener("click", function(evt) {
     if (this.value == "yes") {
-        requireService.style.display = "block";
-    } 
+        form.style.display = "block";
+    }
 });
 
-noNeedVideo.addEventListener("click", function(evt){
-	if (this.value == "no") {
-		requireService.style.display = "none"
-	}
+noNeedVideo.addEventListener("click", function(evt) {
+    if (this.value == "no") {
+        form.style.display = "none"
+    }
 });
 
 
 
-// form.addEventListener("Submit", function(evt) {
+form.addEventListener("submit", function(evt) {
 
-// var inputFirstName = document.getElementById("first-name");
-// var inputLastName = document.getElementById("last-name");
-// var 
+    var firstNameInput = document.getElementById("first-name");
+    var lastNameInput = document.getElementById("last-name");
+    var emailInput = document.getElementById("email");
+    var phoneNumInput = document.getElementById("phone-num");
+
+    if (firstNameInput.checkValidity() == false) {
+        alert("Falta tu nombre");
+        firstNameInput.focus();
+        evt.preventDefault();
+        return false;
+    }
+
+    if (lastNameInput.checkValidity() == false) {
+        alert("Falta tu apellido");
+        lastNameInput.focus();
+        evt.preventDefault();
+        return false;
+    }
+
+    if (emailInput.checkValidity() == false) {
+    	alert("Falta el email");
+    	emailInput.focus();
+    	evt.preventDefault();
+    	return false;
+    }
+
+    if (phoneNumInput.checkValidity() == false) {
+    	alert("Falta el teléfono");
+    	phoneNumInput.focus();
+    	evt.preventDefault();
+    	return false;
+    }
 
 
 
-// });
+
+
+
+
+
+
+
+
+    
+
+
+
+
+});
---
+++ js/menu.js
@@ -0,0 +1,42 @@
+var navbarItems = document.getElementsByClassName('navbar-item');
+
+for (var i = 0; i < navbarItems.length; i++){
+	navbarItems[i].addEventListener('click', function(evt){	
+
+		var sectionToGo = this.getElementsByTagName('a')[0].href.split('#');
+		
+		if(sectionToGo.length > 1) {
+			evt.preventDefault();
+			var goTo = sectionToGo[sectionToGo.length - 1];
+			getElementByIdAndScroll(goTo);
+		}
+	});
+}
+
+function getElementByIdAndScroll(name) {
+	var elem;
+	if (name == '') {
+		elem = document.getElementByid("top");
+	} else {
+		elem = document.getElementById(name);
+	}
+
+	scrollToElement(elem);
+}
+
+function scrollToElement(element) {
+	var jump = parseInt(element.getBoundingClientRect().top * .5);
+	document.body.scrollTop += jump;
+	document.documentElement.scrollTop += jump;
+
+	if (!element.lastJump || element.lastJump > Math.abs(jump)) {
+		element.lastJump = Math.abs(jump);
+
+		setTimeout(function() {
+			scrollToElement(element);
+		}, "60");
+
+	} else {
+		element.lastJump = null;
+	}
+}
